b8768e2165a2462408e925236127f316c4b1d3d3
Daisy-Dawn <agboella20@gmail.com>
2025-05-10 09:42:54 +0100

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Providers from './providers'
 import '@rainbow-me/rainbowkit/styles.css'
+import '@getpara/react-sdk/styles.css'
 
 export const metadata: Metadata = {
     title: 'Connect Button',
